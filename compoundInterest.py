c8294c507a73f398c9eac1c448f313322aec90ad
Kamron Perry <Kamron.Perry@vu.com>
2023-02-17 15:50:11 -0600

    push

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,9 +2,37 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundInterest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+    try:
+        client_one_principal = float((input("Principle (amount): ")))
+        client_one_time =      float((input("Time:               ")))
+        client_one_rate =      float((input("Rate:               ")))
+    except:
+        print("Enter a valid amount for each.")
+    Amount = float((client_one_principal)*(1+client_one_rate/100)**client_one_time)
+    CompoundInterest = Amount - client_one_principal
+    print("Compound Interest: {:.1f}".format(CompoundInterest))
+    print("---")
+    try:
+        client_two_principal = float((input("Principle (amount): ")))
+        client_two_time =      float((input("Time:               ")))
+        client_two_rate =      float((input("Rate:               ")))
+    except:
+        print("Enter a valid amount for each.")
+        exit()
+    Amount = ((client_two_principal)*(1+client_two_rate/100)**client_two_time)
+    CompoundInterestTwo = Amount - client_two_principal
+    print("Compound Interest: {:.2f}".format(CompoundInterestTwo))
+    print("---")
+    try:
+        client_thr_principal = float((input("Principle (amount): ")))
+        client_thr_time =      float((input("Time:               ")))
+        client_thr_rate =      float((input("Rate:               ")))
+    except:
+        print("Enter a valid amount for each.")
+        exit()
+    Amount = ((client_thr_principal)*(1 + client_thr_rate/100)**client_thr_time)
+    CompoundInterestThr = Amount - client_thr_principal
+    print("Compound Interest: {:.1f}".format(CompoundInterestThr))
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
